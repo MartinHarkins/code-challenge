102a47c5113ba51857cd33da7add9e309b111ea6
Gimenitsuj <emigjustin@gmail.com>
2015-07-14 11:34:53 -0500

    Delete unit0.py

--- unit0.py
+++
@@ -1,9 +0,0 @@
-import os
-from os import path
-import re
-
-#Included txt files for future use as a webapp
-mypath = "/home/teitoku/code-challenge/"
-files = [f for f in os.listdir(mypath) if path.isfile(f) and (f.endswith(".html") or f.endswith(".txt"))]
-
-print files
